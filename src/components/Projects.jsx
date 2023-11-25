006c1608d395248fd9ad466123717864d4914e8d
Stephen Allen <Allen.Stephen@student.greenriver.edu>
2023-11-24 21:56:44 -0800

    Changed 'class' to 'className' for proper syntax

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,49 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="project-content">
+        <p>Portfolio</p>
+        <h3>Each Project comes with their own unique ideas</h3>
+        {/* div for all projects */}
+        <div className="projects-display">
+            {/* first project */}
+            <div className="project-left-right">
+                <div className="project-image">
+                    
+                </div>
+                <div className="project-description">
+
+                </div>
+            </div>
+            {/* second project */}
+            <div className="project-right-left">
+                <div className="project-image">
+                    
+                </div>
+                <div className="project-description">
+    
+                </div>
+            </div>
+            {/* third project */}
+            <div className="project-left-right">
+                <div className="project-image">
+                    
+                </div>
+                <div className="project-description">
+
+                </div>
+            </div>
+            {/* fourth project */}
+            <div className="project-right-left">
+                <div className="project-image">
+                    
+                </div>
+                <div className="project-description">
+    
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
