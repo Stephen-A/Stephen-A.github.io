4b1c854bbbc87b0ee6bb20f290490fdc36116c54
Stephen <Allen.Stephen@student.greenriver.edu>
2023-11-24 19:56:18 -0800

    Added layout for projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div class="project-content">
+        <p>Portfolio</p>
+        <h3>Each Project comes with their own unique ideas</h3>
+        {/* div for all projects */}
+        <div class="projects-display">
+            {/* first project */}
+            <div class="project-left-right">
+                <div class="project-image">
+                    
+                </div>
+                <div class="project-description">
+
+                </div>
+            </div>
+            {/* second project */}
+            <div class="project-right-left">
+                <div class="project-image">
+                    
+                </div>
+                <div class="project-description">
+    
+                </div>
+            </div>
+            {/* third project */}
+            <div class="project-left-right">
+                <div class="project-image">
+                    
+                </div>
+                <div class="project-description">
+
+                </div>
+            </div>
+            {/* fourth project */}
+            <div class="project-right-left">
+                <div class="project-image">
+                    
+                </div>
+                <div class="project-description">
+    
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
